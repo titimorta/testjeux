540e088d4fbb7b7786966a6dd54b0630ac950846
titimorta <123767440+titimorta@users.noreply.github.com>
2023-01-27 20:05:33 +0100

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,3 @@
-from distutils.command.config import config
-
 import pygame
 
 from player import Player
@@ -8,17 +6,9 @@ from Map import MapManager
 from src.dialog import DialogBox
 
 
-class CurrentGameState:
-    pass
-
-
 class Game:
 
     def __init__(self):
-
-        self.battle = None
-        self.player = None
-        self.event = None
 
         # definir si notre jeux a commencer
 
@@ -38,23 +28,6 @@ class Game:
         img = pygame.image.load('map/logo.jpg')
         pygame.display.set_icon(img)
 
-    def determine_pokemon_found(self, map_tile):
-        if map_tile not in config.MONSTER_TYPES:
-            return
-
-        random_number = utilities.generate_random_number(1, 10)
-
-        # 20 percent chance of hitting pokemon
-        if random_number <= 2:
-            found_monster = self.monster_factory.create_monster(map_tile)
-            print("you found a monster!")
-            print("Monster Type: " + found_monster.type)
-            print("Attack: " + str(found_monster.attack))
-            print("Health: " + str(found_monster.health))
-
-            self.battle = Battle(self.screen, found_monster, self.player)
-            self.current_game_state = CurrentGameState.BATTLE
-
     def handle_input(self):
         pressed = pygame.key.get_pressed()
 
@@ -72,28 +45,6 @@ class Game:
 
     def update(self):
         self.map_manager.update()
-
-        if self.current_game_state == CurrentGameState.MAP:
-            self.player.move = False
-            self.screen.fill(config.BLACK)
-            # print("update")
-            self.handle_events()
-
-            if self.player_has_moved:
-                self.determine_game_events()
-
-            self.map.render(self.screen, self.player)
-
-        elif self.current_game_state == CurrentGameState.BATTLE:
-            self.battle.update()
-            self.battle.render()
-
-            if self.battle.monster.health <= 0:
-                self.current_game_state = CurrentGameState.MAP
-
-        if self.event is not None:
-            self.event.render()
-            self.event.update()
 
     def run(self):
         clock = pygame.time.Clock()
